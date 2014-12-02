13df9be1693402076751213c2bac0c2b3806a0a7
steward1212 <steward831212@yahoo.com.tw>
2014-12-02 17:11:24 +0800

    Revert "YA"
    This reverts commit 40102653e069518ea3023f501d2dae95538cf4ff.

---
+++ git_lab/src/csclab/tw/Circle.java
@@ -0,0 +1,17 @@
+package csclab.tw;
+
+public class Circle extends Shape {
+	
+	private double radius;
+	
+	public Circle(double radius){
+		this.radius = radius;
+	}
+	
+	@Override
+	public double getArea() {
+		// TODO Auto-generated method stub
+		return this.radius* this.radius* 3.14159;
+	}
+
+}
